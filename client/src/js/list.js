620443ec1a42b097159cbc72dd131fb4c8997cbf
nicoson <nicoson@163.com>
2018-09-26 10:37:28 +0800

    fix tableexport component bug and change to filesaver.js

--- client/src/js/list.js
+++ client/src/js/list.js
@@ -130,7 +130,7 @@ function fillSubTable(ele, datum, info) {
 }
 
 function genExportTable(data) {
-    let temp = `<tbody class="component-hidden">
+    let temp = `<tbody>
                     <tr>
                         <th>序号</th>
                         <th>查处日期</th>
@@ -180,14 +180,10 @@ function genExportTable(data) {
                         <td>${isCompany(USER[data[i].owner].IdentityInfo.type, USER[data[i].owner].IdentityInfo.status)?'企业':'个人'}</td>
                         <td>${USER[data[i].owner].IdentityInfo.enterprise_name}</td>
                         <td>'${USER[data[i].owner].IdentityInfo.enterprise_code}</td>
-                        <td>北京市 海淀区 新东方南楼 733</td>
+                        <td>${USER[data[i].owner].IdentityInfo.contact_address}</td>
                     </tr>`;
     }
     document.querySelector('#wa_list_table_export').innerHTML = temp + '</tbody>';
-    let today = new Date();
-    $('#wa_list_table_export').tableExport({fileName: '上报文件 ' + getDateString(today),formats:['xlsx']});
-    document.querySelector('#wa_list_table_export button').classList.toggle('btn-info');
-    document.querySelector('#wa_list_table_export button').innerHTML = '导出excel文件';
 }
 
 function getDateString(day) {
@@ -232,97 +228,79 @@ function isCompany(type, status) {
 }
 
 
-/*=======================*\
-   excel export component
-\*=======================*/
-// var idTmr;
-// //获取当前浏览器类型
-// function getExplorer() {
-//     var explorer = window.navigator.userAgent;
-//     //ie
-//     if (explorer.indexOf("MSIE") >= 0) {
-//         return 'ie';
-//     }
-//     //firefox
-//     else if (explorer.indexOf("Firefox") >= 0) {
-//         return 'Firefox';
-//     }
-//     //Chrome
-//     else if(explorer.indexOf("Chrome") >= 0){
-//         return 'Chrome';
-//     }
-//     //Opera
-//     else if(explorer.indexOf("Opera") >= 0){
-//         return 'Opera';
-//     }
-//     //Safari
-//     else if(explorer.indexOf("Safari") >= 0){
-//         return 'Safari';
-//     }
-// }
+/*==========================================*\
+   excel export component: use filesaver.js
+\*==========================================*/
+var idTmr;
+//获取当前浏览器类型
+function getExplorer() {
+    var explorer = window.navigator.userAgent;
+    //ie
+    if (explorer.indexOf("MSIE") >= 0) {
+        return 'ie';
+    }
+    //firefox
+    else if (explorer.indexOf("Firefox") >= 0) {
+        return 'Firefox';
+    }
+    //Chrome
+    else if(explorer.indexOf("Chrome") >= 0){
+        return 'Chrome';
+    }
+    //Opera
+    else if(explorer.indexOf("Opera") >= 0){
+        return 'Opera';
+    }
+    //Safari
+    else if(explorer.indexOf("Safari") >= 0){
+        return 'Safari';
+    }
+}
   
-// //获取到类型需要判断当前浏览器需要调用的方法，目前项目中火狐，谷歌，360没有问题
-// function exportExcel(tableid) {
-//     if(getExplorer()=='ie') {
-//         var curTbl = document.getElementById(tableid);
-//         var oXL = new ActiveXObject("Excel.Application");
-//         var oWB = oXL.Workbooks.Add();
-//         var xlsheet = oWB.Worksheets(1);
-//         var sel = document.body.createTextRange();
-//         sel.moveToElementText(curTbl);
-//         sel.select();
-//         sel.execCommand("Copy");
-//         xlsheet.Paste();
-//         oXL.Visible = true;
+//获取到类型需要判断当前浏览器需要调用的方法，目前项目中火狐，谷歌，360没有问题
+function exportExcel(tableid) {
+    if(getExplorer()=='ie') {
+        var curTbl = document.getElementById(tableid);
+        var oXL = new ActiveXObject("Excel.Application");
+        var oWB = oXL.Workbooks.Add();
+        var xlsheet = oWB.Worksheets(1);
+        var sel = document.body.createTextRange();
+        sel.moveToElementText(curTbl);
+        sel.select();
+        sel.execCommand("Copy");
+        xlsheet.Paste();
+        oXL.Visible = true;
 
-//         try {
-//             var fname = oXL.Application.GetSaveAsFilename("Excel.xls", "Excel Spreadsheets (*.xls), *.xls");
-//         } catch (e) {
-//             print("Nested catch caught " + e);
-//         } finally {
-//             oWB.SaveAs(fname);
-//             oWB.Close(savechanges = false);
-//             oXL.Quit();
-//             oXL = null;
-//             idTmr = window.setInterval("Cleanup();", 1);
-//         }
+        try {
+            var fname = oXL.Application.GetSaveAsFilename("Excel.xls", "Excel Spreadsheets (*.xls), *.xls");
+        } catch (e) {
+            print("Nested catch caught " + e);
+        } finally {
+            oWB.SaveAs(fname);
+            oWB.Close(savechanges = false);
+            oXL.Quit();
+            oXL = null;
+            idTmr = window.setInterval("Cleanup();", 1);
+        }
 
-//     } else {
-//         tableToExcel(tableid)
-//     }
-// }
+    } else {
+        fname = `上报文件 ${getDateString(new Date())}.xls`;
+        tableToExcel(tableid, fname)
+    }
+}
 
-// function Cleanup() {
-//     window.clearInterval(idTmr);
-//     CollectGarbage();
-// }
+function Cleanup() {
+    window.clearInterval(idTmr);
+    CollectGarbage();
+}
 
-// //判断浏览器后调用的方法，把table的id传入即可
-// var tableToExcel = (function() {
-//     var uri = 'data:application/vnd.ms-excel;base64,';
-//     var template = '<html><head><meta charset="UTF-8"></head><body><table>{table}</table></body></html>';
-    
-//     var base64 = function(s) {
-//         return window.btoa(unescape(encodeURIComponent(s)))
-//     };
-
-//     var format = function(s, c) {
-//         return s.replace(/{(\w+)}/g, function(m, p) {
-//             return c[p];
-//         })
-//     };
-
-//     return function(table, name) {
-//         // if (!table.nodeType) table = document.getElementById(table)
-//         var ctx = {
-//             worksheet: name || 'Worksheet',
-//             table: document.getElementById(table).innerHTML
-//         }
-//         window.location.href = uri + base64(format(template, ctx));
-//         // document.getElementById(table).href = uri + base64(format(template, ctx));
-//         // document.getElementById(table).download = '违章报告'+(new Date()).getTime() + '.xls';
-//         // document.getElementById(table).click();
-//     }
-// })()
+//判断浏览器后调用的方法，把table的id传入即可
+function tableToExcel(table, name) {
+    var template = `<html><head><meta charset="UTF-8"></head><body><table>${document.getElementById(table).innerHTML}</table></body></html>`
+    var blob = new Blob([template], {
+        type: "application/vnd.ms-excel;charset=charset=utf-8"
+    });
+    saveAs(blob, name);
+}
 
 
--- server/model/schedulejob.js
+++ server/model/schedulejob.js
@@ -1,9 +1,12 @@
 const schedule = require('node-schedule');
 const fusionHelper = require('./fusionhelper');
+const atlabHelper = require('./atlabhelper');
 const fh = new fusionHelper();
+const ah = new atlabHelper();
 
 class scheduleJob {
     constructor() {
+        this.batchsize = 200;
         var j = schedule.scheduleJob('0 0 1 * * *', function(){
             console.log('job 1 works!');
         });
@@ -18,6 +21,17 @@ class scheduleJob {
 
     updateURLTable() {
         fh.updateURL();
+    }
+
+    updateURLTableByAPI() {
+        let endTime = [4,0,0];
+        let today = new Date();
+        let date = [today.getFullYear(), today.getMonth(), today.getDate()];
+        if(new Date().getTime < new Date(...date, ...endTime)) {
+            ah.censorBatch(this.batchsize).then(e => {
+                this.updateURLTableByAPI();
+            });
+        }
     }
 }
 
